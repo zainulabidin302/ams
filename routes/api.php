9e411c8e4538bab238aee277e35ff35d1bb6f74b
Anas Majeed <anasmajeed.ch@gmail.com>
2018-11-17 02:17:37 +0500

    CorsAndApi

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,22 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use App\User;
+Route::group(['middleware'=>['cors']],function(){
+  Route::get('/users',function(){
+    return response()->json(
+      User::all()
+    );
+  });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  Route::post('/users',function(Request $request){
+    // return $request;
+    $user = new User();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = $request->password;
+    $user->save();
+  });
+});
+Route::group(['middleware'=> ['auth:api'] ],function(){
 });
